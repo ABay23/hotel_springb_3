8089bbf14064a88245bc876e81d1eb4c298b876a
ABay23 <alejandrobay23@gmail.com>
2024-01-16 23:50:45 -0500

    DB and Docker Installed

--- Bin/start_postgres.sh
+++ Bin/start_postgres.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-mkdir -p "$HOME"/.local/docker/postgresql
+#mkdir -p "$HOME"/.local/docker/postgresql
 
 docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=local -d -p 5432:5432 -e PGDATA=/var/lib/postgresql/data/pgdata -v "$HOME"/.local/docker/postgresql/data:/var/lib/postgresql/data postgres

76c502d2c709ee0e1b1dd554fe2eadcae23b1ec8
ABay23 <alejandrobay23@gmail.com>
2024-01-20 00:48:34 -0500

    New page Occupancy

---
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/controller/OccupancyController.java
@@ -0,0 +1,42 @@
+package com.abay23.landonhotel.web.controller;
+
+import com.abay23.landonhotel.service.RoomReservationService;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+@Controller
+@RequestMapping("/occupancy")
+public class OccupancyController {
+
+    private final RoomReservationService service;
+
+    public OccupancyController(RoomReservationService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getOccupancy(Model model, @RequestParam(value = "date", required = false) String dateString){
+        Date date = new Date();
+        if (StringUtils.isNotBlank(dateString)){
+            try {
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                date = (Date) format.parse(dateString);
+            }
+            catch (Exception e){
+                //do nothing ???
+            }
+        }
+        model.addAttribute("date", date);
+        model.addAttribute("reservations", this.service.getRoomReservationForDate(dateString));
+        return "occupancy";
+
+    }
+}
+

62178681cedc1df1fc2e67875348ba04b9cd92ba
ABay23 <alejandrobay23@gmail.com>
2024-01-22 11:27:23 -0500

    Updated Connection

--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/RoomApiController.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/RoomApiController.java
@@ -2,7 +2,7 @@ package com.abay23.landonhotel.web.api;
 
 import com.abay23.landonhotel.data.entity.Room;
 import com.abay23.landonhotel.data.repository.RoomRepository;
-import com.abay23.landonhotel.web.exception.BadResquestException;
+import com.abay23.landonhotel.web.exception.BadRequestException;
 import com.abay23.landonhotel.web.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -34,7 +34,7 @@ public class RoomApiController {
     public Room getRoom(@PathVariable("id") long id){
         Optional<Room> room = this.roomRepository.findById(id);
         if (room.isEmpty()){
-            throw new NotFoundException("Room not found wiht id: " + id);
+            throw new NotFoundException("Room not found with id: " + id);
         }
         return room.get();
     }
@@ -42,7 +42,7 @@ public class RoomApiController {
     @PutMapping("/{id}")
     public Room updatedRoom(@PathVariable("id") long id, @RequestBody Room room){
         if (id != room.getId()){
-            throw new BadResquestException("id on path doesn't match body");
+            throw new BadRequestException("id on path doesn't match body");
         }
         return this.roomRepository.save(room);
     }
--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/exception/BadRequestException.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/exception/BadRequestException.java
@@ -4,20 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadResquestException extends RuntimeException{
-    public BadResquestException() {
+public class BadRequestException extends RuntimeException{
+    public BadRequestException() {
         super();
     }
 
-    public BadResquestException(String message) {
+    public BadRequestException(String message) {
         super(message);
     }
 
-    public BadResquestException(String message, Throwable cause) {
+    public BadRequestException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public BadResquestException(Throwable cause) {
+    public BadRequestException(Throwable cause) {
         super(cause);
     }
 

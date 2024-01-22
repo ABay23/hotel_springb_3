53a5935582647f7893ad9d69186b0da670b35e39
ABay23 <alejandrobay23@gmail.com>
2024-01-21 23:10:07 -0500

    More Restful messaging

--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/RoomApiController.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/RoomApiController.java
@@ -2,10 +2,13 @@ package com.abay23.landonhotel.web.api;
 
 import com.abay23.landonhotel.data.entity.Room;
 import com.abay23.landonhotel.data.repository.RoomRepository;
+import com.abay23.landonhotel.web.exception.BadResquestException;
+import com.abay23.landonhotel.web.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rooms")
@@ -24,6 +27,29 @@ public class RoomApiController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Room createRoom(@RequestBody Room room){
-       returns this.roomRepository.save(room);
+       return this.roomRepository.save(room);
+    }
+
+    @GetMapping("/{id}")
+    public Room getRoom(@PathVariable("id") long id){
+        Optional<Room> room = this.roomRepository.findById(id);
+        if (room.isEmpty()){
+            throw new NotFoundException("Room not found wiht id: " + id);
+        }
+        return room.get();
+    }
+
+    @PutMapping("/{id}")
+    public Room updatedRoom(@PathVariable("id") long id, @RequestBody Room room){
+        if (id != room.getId()){
+            throw new BadResquestException("id on path doesn't match body");
+        }
+        return this.roomRepository.save(room);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.RESET_CONTENT)
+    public void deleteRoom(@PathVariable("id") long id){
+        this.roomRepository.deleteById(id);
     }
 }

8726f488cf35a8e91cf9e66c9fb4a6cef41dfb6c
ABay23 <alejandrobay23@gmail.com>
2024-01-22 22:51:41 -0500

    Reservation Controller

--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/ReservationApiController.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/ReservationApiController.java
@@ -1,4 +1,25 @@
 package com.abay23.landonhotel.web.api;
 
+import com.abay23.landonhotel.data.entity.Reservation;
+import com.abay23.landonhotel.data.repository.ReservationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
 public class ReservationApiController {
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationApiController(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @GetMapping
+    public List<Reservation> getAllReservations(){
+        return this.reservationRepository.findAll();
+    }
 }

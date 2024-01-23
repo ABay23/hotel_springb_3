6854ff589b7cf9e5e8805d7ad6a297059b0a8aaf
ABay23 <alejandrobay23@gmail.com>
2024-01-22 22:59:57 -0500

    Get single reservation

--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/ReservationApiController.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/ReservationApiController.java
@@ -2,10 +2,13 @@ package com.abay23.landonhotel.web.api;
 
 import com.abay23.landonhotel.data.entity.Reservation;
 import com.abay23.landonhotel.data.repository.ReservationRepository;
+import com.abay23.landonhotel.web.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/reservation")
@@ -26,5 +29,14 @@ public class ReservationApiController {
     @ResponseStatus(HttpStatus.CREATED)
     public Reservation createReservation(@RequestBody Reservation reservation){
         return this.reservationRepository.save(reservation);
+    }
+
+    @GetMapping("/{id}")
+    public Reservation getReservation(@PathVariable("id") long id){
+        Optional<Reservation> reservation = this.reservationRepository.findById(id);
+        if ( reservation.isEmpty()){
+            throw new NotFoundException("No reservation was found by id: " + id);
+        }
+        return reservation.get();
     }
 }

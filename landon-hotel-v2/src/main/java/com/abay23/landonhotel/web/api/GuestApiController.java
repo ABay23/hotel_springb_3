46e061a3c087c702eec9fbcd3a60603cddc0fab8
ABay23 <alejandrobay23@gmail.com>
2024-01-22 22:34:55 -0500

    Get single guest

--- landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/GuestApiController.java
+++ landon-hotel-v2/src/main/java/com/abay23/landonhotel/web/api/GuestApiController.java
@@ -2,10 +2,12 @@ package com.abay23.landonhotel.web.api;
 
 import com.abay23.landonhotel.data.entity.Guest;
 import com.abay23.landonhotel.data.repository.GuestRepository;
+import com.abay23.landonhotel.web.exception.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/guest")
@@ -26,5 +28,14 @@ public class GuestApiController {
     @ResponseStatus(HttpStatus.CREATED)
     public Guest createGuest(@RequestBody Guest guest){
         return this.guestRepository.save(guest);
+    }
+
+    @GetMapping("/{id}")
+    public Guest getGuest(@PathVariable("id") long id){
+        Optional<Guest> guest = this.guestRepository.findById(id);
+        if (guest.isEmpty()){
+            throw new BadRequestException("Guest Not found with id: " + id);
+        }
+        return guest.get();
     }
 }
